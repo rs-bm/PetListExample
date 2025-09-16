fd885a04a510e3215c6ed9d15d4a93fa00e04c9d
williamswanson <william.swanson@bruins.belmont.edu>
2025-09-16 11:36:12 -0500

    steps 1-4

---
+++ app/src/main/java/model/Pet.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Pet {
+    String name;
+    String breed;
+    int age;
+
+    public Pet(String name, String breed, int age) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+    }
+}
