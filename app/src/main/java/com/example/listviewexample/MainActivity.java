91838876553f53142d04527feeb8bd48373bc438
williamswanson <william.swanson@bruins.belmont.edu>
2025-09-18 11:49:38 -0500

    added two line display

--- app/src/main/java/com/example/listviewexample/MainActivity.java
+++ app/src/main/java/com/example/listviewexample/MainActivity.java
@@ -3,23 +3,40 @@ package com.example.listviewexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
 
 import model.Pet;
 
 public class MainActivity extends AppCompatActivity {
     ListView petListView;
+    LinkedList<Pet> pets;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         petListView = findViewById(R.id.petListView);
+        petListView.setOnItemSelectedListener(listListener);
 
-        int selection = 2;
+        Random rand = new Random();
+        pets = new LinkedList<>();
+        pets.add(new Pet("dog", "Golden Retriever", rand.nextInt(20)));
+        pets.add(new Pet("dog 2", "Beagle", rand.nextInt(20)));
+        pets.add(new Pet("dog 3", "Checkoslovaquian Wolf Dog", rand.nextInt(20)));
+
+        int selection = 3;
         populateListView(selection);
     }
 
@@ -31,29 +48,52 @@ public class MainActivity extends AppCompatActivity {
             case 2:
                 usingDataArray();
                 break;
-
+            case 3:
+                usingTwoLines();
+                break;
         }
     }
 
-    /**
-     * This method populates the list view using a string resource containing
-     * the content of a strings array resource
-     */
     private void useStringResource() {
         String[] content = getResources().getStringArray(R.array.breeds);
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, content);
         petListView.setAdapter(adapter);
     }
 
-    /**
-     * This method populates the list view using a string resource containing the list of a data array
-     */
     private void usingDataArray() {
-        LinkedList<Pet> content  = new LinkedList<>();
-        content.add(new Pet("dog", "Golden Retriever", 5));
-        content.add(new Pet("doug", "Beagle", 10));
-        content.add(new Pet("god", "Checkoslovaquian Wolf Dog", 15));
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, content);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, pets);
         petListView.setAdapter(adapter);
     }
+
+    private void usingTwoLines() {
+        List<HashMap<String, String>> data = new ArrayList<>();
+
+        for (Pet p : pets) {
+            HashMap<String, String> current = new HashMap<>();
+            current.put("name", p.getName());
+            current.put("breed", p.getBreed());
+            data.add(current);
+        }
+
+        SimpleAdapter adapter = new SimpleAdapter(
+                this,
+                data,
+                android.R.layout.simple_list_item_2,
+                new String[]{"name", "breed"},
+                new int[]{android.R.id.text1, android.R.id.text2}
+        );
+        petListView.setAdapter(adapter);
+    }
+
+    AdapterView.OnItemSelectedListener listListener = new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+            Toast.makeText(getApplicationContext(), view.toString() + " " + i, Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> adapterView) {
+
+        }
+    };
 }
--- app/src/main/java/model/Pet.java
+++ app/src/main/java/model/Pet.java
@@ -14,4 +14,27 @@ public class Pet {
         return (name + " (" + breed + "): " + age);
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
